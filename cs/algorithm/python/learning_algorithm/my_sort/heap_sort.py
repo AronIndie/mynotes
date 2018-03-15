a24f03e22af47d9af7053e87f3dbe48387c52b15
yangruipis <yangruipis@163.com>
2018-03-15 22:54:38 +0800

    20180315

---
+++ cs/algorithm/python/learning_algorithm/my_sort/heap_sort.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+
+"""
+ref: https://www.cnblogs.com/chengxiao/p/6129630.html
+
+记住：
+非叶子节点的数目 = len(heap) // 2 - 1
+给定非叶子节点i，其子节点索引为 2*i +1 和 2*i+2
+"""
+
+
+def heap_sort(a: list):
+    for i in range(len(a) - 1, -1, -1):
+        # 从第一个非叶子节点，从上至下，从右至左调整
+        a = adj_heap(a, i)
+
+    for j in range(len(a) - 1, 0, -1):
+        a[0], a[j] = a[j], a[0]
+        a[:j] = adj_heap(a[:j], 0)
+    return a
+
+
+def adj_heap(a, i):
+    """
+    调整数组使其满足堆的性质
+    :param a: 数组
+    :param i: 第i个叶子节点
+    :return: void
+    """
+    temp = a[i]
+    k = 2 * i + 1
+    while k < len(a):
+        # 判断右节点和左节点那个大，如果右节点更大，则把k指向其
+        if k + 1 < len(a) and a[k + 1] > a[k]:
+            k += 1
+
+        # 如果子节点值更小，则下沉，不用交换，类似插入排序
+        if temp < a[k]:
+            a[i] = a[k]
+            i = k
+        else:
+            break
+        k = 2 * k + 1
+    a[i] = temp
+    return a
+
+
+if __name__ == '__main__':
+    a = [1,3,2,6,5,4]
+    print(heap_sort(a))
--- cs/datastruct/python/tree/binary_tree_traverse.py
+++ cs/datastruct/python/tree/binary_tree_traverse.py
@@ -3,12 +3,15 @@
 """
 - 深度优先： 前序、中序、后序，需要调用栈，python3多重继承原则：从左到右，深度优先
 - 广度优先： 调用队列， python2多重继承原则
+
+ref:
+
 """
 class node:
 
     __slot__ = ['value', 'left', 'right']
 
-    def __init__(self, value: str, left: node, right: node):
+    def __init__(self, value: int, left, right):
         self.value = value
         self.left = left
         self.right = right
@@ -22,32 +25,64 @@ def get_tree() -> node:
     root.left.left = node(3, None, None)
     root.left.right = node(4, None, None)
     root.right.left = node(5, None, None)
+    root.right.right = node(6, None, None)
     return root
 
 
 def pre_traverse_recursion(root: node):
-
+    if not node:
+        return
     print(root.value)
-    pre_traverse_norecursion(root.right)
-    pre_traverse_norecursion(root.right)
+    pre_traverse_recursion(root.right)
+    pre_traverse_recursion(root.right)
 
 
-def pre_traverse_norecursion(root: node):
+def pre_order_traverse_norecursion(root: node):
     """
-    调用栈
+    先序遍历，调用栈
     """
     stack = []
     stack.append(root)
-
     while len(stack) != 0:
         this_node = stack.pop(0)
-
         print(this_node.value)
+        if this_node.right != None:
+            stack.insert(0, this_node.right)
         if this_node.left != None:
             stack.insert(0, this_node.left)
-        if this_node.left != None:
-            stack.insert(0, this_node.right)
 
+
+def in_order_traverse_norecursion(root: node):
+    """
+    中序遍历，调用栈
+    """
+    stack = []
+    temp = root
+    while len(stack) != 0 or temp:
+        if temp:
+            stack.insert(0, temp)
+            temp = temp.left
+        else:
+            temp = stack.pop(0)
+            print(temp.value)
+            temp = temp.right
+
+
+def post_order_traverse_norecursion(root: node):
+    """
+    后序遍历，双栈法
+    """
+    stack1 = [root]
+    stack2 = []
+    while len(stack1) != 0:
+        temp = stack1.pop(0)
+        stack2.insert(0, temp)
+        if temp.left:
+            stack1.insert(0, temp.left)
+        if temp.right:
+            stack1.insert(0, temp.right)
+    while len(stack2) != 0:
+        print(stack2.pop(0).value)
 
 def breadth_first_traverse(root: node):
     """
@@ -67,3 +102,6 @@ def breadth_first_traverse(root: node):
             queue.insert(0, this_node.left)
 
 
+if __name__ == '__main__':
+    node1 = get_tree()
+    post_order_traverse_norecursion(node1)
