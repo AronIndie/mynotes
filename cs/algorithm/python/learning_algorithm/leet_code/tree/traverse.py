d301c76892302020958a6f48ae5c82d8cfd6c5c8
yangruipis <yangruipis@163.com>
2018-04-13 21:52:57 +0800

    20180413

--- cs/algorithm/python/learning_algorithm/leet_code/tree/traverse.py
+++ cs/algorithm/python/learning_algorithm/leet_code/tree/traverse.py
@@ -29,19 +29,22 @@ def pre_order_traverse(root):
     pre_order_traverse(root.left)
     pre_order_traverse(root.right)
 
+
 def in_order_traverse(root):
     if not root:
         return
-    pre_order_traverse(root.left)
+    in_order_traverse(root.left)
     print(root.value)
-    pre_order_traverse(root.right)
+    in_order_traverse(root.right)
+
 
 def post_order_traverse(root):
     if not root:
         return
-    pre_order_traverse(root.left)
-    pre_order_traverse(root.right)
+    post_order_traverse(root.left)
+    post_order_traverse(root.right)
     print(root.value)
+
 
 def pre_order_traverse_NC(root):
     stack = [root]
@@ -53,15 +56,67 @@ def pre_order_traverse_NC(root):
         if temp.left:
             stack.append(temp.left)
 
+
 def in_order_traverse_NC(root):
     stack = []
-    
+    temp = root
+    while temp or stack:
+        if temp:
+            stack.append(temp)
+            temp = temp.left
+        else:
+            temp = stack.pop()
+            print(temp.value)
+            temp = temp.right
+
+
+def post_order_traverse_NC1(root):
+    stack1 = [root]
+    stack2 = []
+    while stack1:
+        temp = stack1.pop()
+        stack2.append(temp)
+        if temp.left:
+            stack1.append(temp.left)
+        if temp.right:
+            stack2.append(temp.right)
+    while stack2:
+        print(stack2.pop())
+
+def post_order_traverse_NC2(root):
+    stack = [root]
+    bool_dic = {}
+    while stack:
+        node = stack[-1]
+        if (not node.left and not node.right) or node in bool_dic:
+            print(stack.pop().value)
+
+        if node not in bool_dic:
+            bool_dic[node] = True
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+
+
+def breadth_first_traverse(root):
+    queue = [root]
+
+    while queue:
+        temp = queue.pop(0)
+        print(temp.value)
+        if temp.left:
+            queue.append(temp.left)
+        if temp.right:
+            queue.append(temp.right)
+
 
 if __name__ == '__main__':
-    arr = [1,2,3,4,5,6,7,8]
-    root = gen_tree(arr)
-    print(root)
-    print(pre_order_traverse(root))
-    print(pre_order_traverse_NC(root))
+    arr = [1,2,3,4,5,6,7,8,9,10]
+    theRoot = gen_tree(arr)
+    print(post_order_traverse(theRoot))
+    # print(pre_order_traverse(root))
+    # print(post_order_traverse_NC2(root))
+    print(breadth_first_traverse(theRoot))
 
 
