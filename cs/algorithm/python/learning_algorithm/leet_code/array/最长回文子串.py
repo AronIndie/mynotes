c25b3d459567a0ef7739d6a4ed1286d58c95216c
yangruipis <yangruipis@163.com>
2018-06-01 11:48:40 +0800

    20180601

---
+++ cs/algorithm/python/learning_algorithm/leet_code/array/最长回文子串.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+
+def foo(arr):
+    l = len(arr)
+    res =[[0 for i in range(l)] for j in range(l)]
+    for i in range(l):
+        for j in range(i, l):
+            if i == j
